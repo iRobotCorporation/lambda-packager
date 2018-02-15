461fdd5cae6ba269bafe742fc320c06f85cb76fe
Unknown <bkehoe@irobot.com>
2018-02-15 06:38:04 -0500

    Only query AWS if needed, change bucket format

--- lambda_packager/__main__.py
+++ lambda_packager/__main__.py
@@ -3,13 +3,13 @@ import pkg_resources
 import sys
 import argparse
 
-def run_make(action, bucket=None, bucket_suffix=None, make_args=[]):
+def run_make(action, bucket=None, bucket_prefix=None, make_args=[]):
     with pkg_resources.resource_stream(__name__, 'Makefile') as fp:
         args = ['make', '-f', '-']
         if bucket:
             args.append('BUCKET={}'.format(bucket))
-        if bucket_suffix:
-            args.append('BUCKET_SUFFIX={}'.format(bucket_suffix))
+        if bucket_prefix:
+            args.append('BUCKET_PREFIX={}'.format(bucket_prefix))
         args.append(action)
         args.extend(make_args)
         return subprocess.call(args, stdin=fp)
@@ -19,13 +19,13 @@ def main():
     
     parser.add_argument('action', choices=['config_from_template', 'build', 'upload', 'clean'])
     parser.add_argument('--bucket')
-    parser.add_argument('--bucket_suffix')
+    parser.add_argument('--bucket_prefix')
     
-    args, other_args = parser.parse_know_args()
+    args, other_args = parser.parse_known_args()
     
     sys.exit(run_make(args.action,
                       bucket=args.bucket,
-                      bucket_suffix=args.bucket_suffix,
+                      bucket_prefix=args.bucket_prefix,
                       make_args=other_args))
 
 if __name__ == '__main__':
